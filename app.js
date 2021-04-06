4987ff57430a4addd2b75ccf8f1958181e773238
maxly20 <maxly2011@gmail.com>
2021-04-06 16:32:12 +1000

    building tab

--- app.js
+++ app.js
@@ -44,3 +44,24 @@ function slidefun(n) {
   myslide[counter - 1].style.display = 'block';
   dot[counter - 1].className += ' active';
 }
+//===========================
+//      FEATURES TABS
+//===========================
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+const tabs = $$('.tab-item');
+const panes = $$('.tab-pane');
+
+const tabActive = $('.tab-item.active');
+
+tabs.forEach((tab, index) => {
+  const pane = panes[index];
+
+  tab.onclick = function () {
+    $('.tab-item.active').classList.remove('active');
+    $('.tab-pane.active').classList.remove('active');
+    this.classList.add('active');
+    pane.classList.add('active');
+  };
+});
